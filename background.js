c39bf345e77eb0e33786254fd964d76babbbe35f
paullee100 <leepaul863@gmail.com>
2023-03-09 16:40:32 -0800

    remove from png

--- background.js
+++ background.js
@@ -2,25 +2,6 @@ class Fence {
     constructor(game, x, y, state) {
         Object.assign(this, {game, x, y, state});
 
-        this.spritesheet = [];
-
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence00.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence01.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence02.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence03.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence04.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence05.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence06.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence07.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence08.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence09.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence10.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence11.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence12.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence13.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence14.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/fence/fence15.png"));
-
         this.BB = new BoundingBox(this.x, this.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH);
     };
 
@@ -33,7 +14,6 @@ class Fence {
             ctx.strokeStyle = "red";
             ctx.strokeRect(this.x - this.game.camera.x, this.y - this.game.camera.y, 16, 16);
         }
-        // ctx.drawImage(this.spritesheet[this.state], this.x - this.game.camera.x, this.y - this.game.camera.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH);
     };
 }
 
@@ -47,7 +27,7 @@ class Ground {
     };
 
     draw(ctx) {
-        ctx.drawImage(ASSET_MANAGER.getAsset(this.image), this.x - this.game.camera.x, this.y - this.game.camera.y, 1024 * PARAMS.SCALE, 1024 * PARAMS.SCALE);
+
     };
 }
 
@@ -55,23 +35,13 @@ class GroundDecor {
     constructor(game, image, x, y) {
         Object.assign(this, {game, image, x, y});
 
-        this.spritesheet = [];
-
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/ground_decor/decor0.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/ground_decor/decor1.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/ground_decor/decor2.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/ground_decor/decor3.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/ground_decor/decor4.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/ground_decor/decor5.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/ground_decor/decor6.png"));
-        this.spritesheet.push(ASSET_MANAGER.getAsset("./sprites/ground_decor/decor7.png"));
-    };
+        };
 
     update() {
 
     };
 
     draw(ctx) {
-        ctx.drawImage(ASSET_MANAGER.getAsset(this.image), this.x - this.game.camera.x, this.y - this.game.camera.y, PARAMS.BLOCKWIDTH, PARAMS.BLOCKWIDTH);
+
     };
 }
--- main.js
+++ main.js
@@ -17,80 +17,8 @@ ASSET_MANAGER.queueDownload("./sprites/slime_dead.png");
 
 // background //
 
-// fence
-ASSET_MANAGER.queueDownload("./sprites/fence/fence00.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence01.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence02.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence03.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence04.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence05.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence06.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence07.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence08.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence09.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence10.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence11.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence12.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence13.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence14.png");
-ASSET_MANAGER.queueDownload("./sprites/fence/fence15.png");
-
 // ground
 ASSET_MANAGER.queueDownload("./sprites/ground/background.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground00.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground01.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground02.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground03.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground04.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground05.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground06.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground07.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground08.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground09.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground10.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground11.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground12.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground13.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground14.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground15.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground16.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground17.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground18.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground19.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground20.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground21.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground22.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground23.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground24.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground25.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground26.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground27.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground28.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground29.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground30.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground31.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground32.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground33.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground34.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground35.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground36.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground37.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground38.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground39.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground40.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground41.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground42.png");
-ASSET_MANAGER.queueDownload("./sprites/ground/ground43.png");
-
-// ground decor
-ASSET_MANAGER.queueDownload("./sprites/ground_decor/decor0.png");
-ASSET_MANAGER.queueDownload("./sprites/ground_decor/decor1.png");
-ASSET_MANAGER.queueDownload("./sprites/ground_decor/decor2.png");
-ASSET_MANAGER.queueDownload("./sprites/ground_decor/decor3.png");
-ASSET_MANAGER.queueDownload("./sprites/ground_decor/decor4.png");
-ASSET_MANAGER.queueDownload("./sprites/ground_decor/decor5.png");
-ASSET_MANAGER.queueDownload("./sprites/ground_decor/decor6.png");
-ASSET_MANAGER.queueDownload("./sprites/ground_decor/decor7.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	PARAMS.BLOCKWIDTH = PARAMS.BITWIDTH * PARAMS.SCALE;
